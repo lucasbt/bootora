70a437555b297d81fb4b1bf0c5e58e566718bdf9
Lucas Bittencourt <lucasbt@gmail.com>
2025-11-13 11:30:10 -0300

    fix: export function

--- modules/fancy.sh
+++ modules/fancy.sh
@@ -110,7 +110,7 @@ WALLS_FOLDERS=(
 
 execute_fancy_module() {
 	log_subheader "System install and config aesthetics"
-	source install_dnf_fonts
+	export install_dnf_fonts
 	spinner_run "Installing preferred fonts from DNF. Please wait, this may take a while..." install_dnf_fonts
 	install_nerd_fonts
 	install_urls_fonts
